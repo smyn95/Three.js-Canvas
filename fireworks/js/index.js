2a8b6f7c6bc6552e68376c21666765035ff3ad3d
smyn95 <tlsalduszz@gmail.com>
2023-05-01 19:42:07 +0900

    보일러 플레이트를 위한 함수 분리

---
+++ fireworks/js/index.js
@@ -0,0 +1,49 @@
+import CanvasOption from "./canvasOption.js";
+
+let canvasWidth, canvasHeight;
+
+class Canvas extends CanvasOption {
+  constructor() {
+    super();
+  }
+
+  init() {
+    this.canvasWidth = innerWidth;
+    this.canvasHeight = innerHeight;
+    this.canvas.width = this.canvasWidth * this.dpr;
+    this.canvas.height = this.canvasHeight * this.dpr;
+    this.ctx.scale(this.dpr, this.dpr);
+
+    this.canvas.style.width = this.canvasWidth + "px";
+    this.canvas.style.height = this.canvasHeight + "px";
+  }
+  
+  render() {
+    let now, delta;
+    let then = Date.now();
+
+    const frame = () => {
+      requestAnimationFrame(frame);
+      now = Date.now();
+      delta = now - then;
+
+      if (delta < this.interval) return;
+
+      canvas.ctx.fillRect(100, 100, 200, 200);
+
+      then = now - (delta % this.interval);
+    };
+    requestAnimationFrame(frame);
+  }
+}
+
+const canvas = new Canvas();
+
+window.addEventListener("load", () => {
+  canvas.init();
+  canvas.render();
+});
+
+window.addEventListener("resize", () => {
+  canvas.init();
+});
