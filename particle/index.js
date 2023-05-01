41b020ce68488448e44e195bbd0833d054cfa731
smyn95 <tlsalduszz@gmail.com>
2023-05-01 16:05:45 +0900

    dat.GUI

--- particle/index.js
+++ particle/index.js
@@ -16,6 +16,43 @@ ctx.scale(dpr, dpr);
 
 // ctx.fillRect(10, 10, 50, 50);
 
+const feGaussianBlur = document.querySelector("feGaussianBlur");
+const feColorMatrix = document.querySelector("feColorMatrix");
+
+const controls = new (function () {
+  this.blurValue = 40;
+  this.alphaChennel = 100;
+  this.alphaOffset = -23;
+  this.acc = 1.03;
+})();
+
+let gui = new dat.GUI();
+
+const f1 = gui.addFolder("gooey Effect");
+f1.open();
+f1.add(controls, "blurValue", 0, 100).onChange((value) => {
+  feGaussianBlur.setAttribute("stdDeviation", value);
+});
+
+f1.add(controls, "alphaChennel", 1, 100).onChange((value) => {
+  feColorMatrix.setAttribute(
+    "values",
+    `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${value} ${controls.alphaOffset}`
+  );
+});
+
+f1.add(controls, "alphaOffset", -40, 40).onChange((value) => {
+  feColorMatrix.setAttribute(
+    "values",
+    `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${controls.alphaChennel} ${value}`
+  );
+});
+
+const f2 = gui.addFolder("particle Property");
+f2.add(controls, "acc", 1, 1.5, 0.01).onChange((value) => {
+  particles.forEach((particle) => (particle.acc = value));
+});
+
 class Particle {
   constructor(x, y, radius, vy) {
     this.x = x;
